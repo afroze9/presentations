09fc197d02665c9df2a7812a67a2e8ea6f27e067
Afroze Amjad <afroze.amjad@confiz.com>
2023-12-15 13:36:44 +0500

    01 update

--- 01-domain-driven-design/components/Progress.vue
+++ 01-domain-driven-design/components/Progress.vue
@@ -1,118 +1,52 @@
 <template>
-  <div class="progress" :style="{ width, height }">
-    <svg
-      viewBox="0 0 96 96"
-      class="svg-circle-progress"
-      style="width: 96px; height: 96px"
-    >
-      <circle
-        r="40"
-        cx="48"
-        cy="48"
-        fill="none"
-        stroke-miterlimit="20"
-        stroke-width="10"
-        class="svg-progress"
-        style="stroke-dasharray: 275, 279.602; stroke: #eee"
-      ></circle>
-      <circle
-        r="40"
-        cx="48"
-        cy="48"
-        fill="none"
-        stroke-miterlimit="20"
-        stroke-width="10"
-        class="svg-progress"
-        :style="`stroke-dasharray: ${progressValue}, 279.602;stroke:${color};`"
-      ></circle>
-    </svg>
-    <div class="mask" >
-      <span class="bigData"> {{ targetValue }} %</span> 
-    </div>
-  </div>
+  <LightOrDark>
+    <template #light>
+      <div class="progress-container-light">
+        <div class="progress" :style="{ width: percentage + '%' }"></div>
+      </div>
+    </template>
+    <template #dark>
+      <div class="progress-container-dark">
+        <div class="progress" :style="{ width: percentage + '%' }"></div>
+      </div>
+    </template>
+  </LightOrDark>
 </template>
 
-<script setup lang="ts">
-
-import { ref, toRefs, watch, onMounted } from 'vue'
-
-const props = defineProps({
-  targetValue: {
-    type: Number,
-    require: true,
-    default: -1
-  },
-  color: {
-    type: String,
-    default: '#4c7cee'
-  },
-  width: {
-    type: String,
-    default: '100px'
-  },
-  height: {
-    type: String,
-    default: '100px'
-  }
-})
-const { height, width, color, targetValue } = toRefs(props)
-
-const showProgress = ref<number>(0)
-const addData = () => {
-  if (targetValue.value === 0) return
-  let timer = setInterval(() => {
-    if (targetValue.value === showProgress.value) {
-      clearInterval(timer), timer == null
-      return
+<script>
+export default {
+  props: {
+    percentage: {
+      type: Number,
+      required: true,
+      validator: value => value >= 0 && value <= 100
     }
-    ++showProgress.value
-  }, 15)
-}
-onMounted(() => {
-  addData()
-})
-const progressValue = ref<number>((showProgress.value / 100) * 250)
-
-watch(
-  targetValue,
-  (newValue: number) => {
-    console.log(newValue)
-    progressValue.value = (newValue / 100) * 250
-  },
-  {
-    immediate: true
   }
-)
+};
 </script>
 
-<style lang="less" scoped>
+<style scoped>
+.progress-container-light {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: #f0f0f0;
+}
+
+.progress-container-dark {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: #000000;
+}
+
 .progress {
-  display: inline-block;
-  position: relative;
-  height: 100px;
-  text-align: center;
-  transform: scale(0.5);
-}
-.svg-circle-progress {
-  position: relative;
-  transform: rotate(-90deg);
-}
-.svg-progress {
-  stroke: #2196f3;
-  stroke-linecap: round;
-  transition: all 0.3s linear;
-}
-.mask {
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  width:100px;
-  transform: translate(-50%, -50%);
-  .bigData {
-    font-size: 25px;
-    font-weight: 500;
-    color: #232323;
-    
-  }
+  height: 100%;
+  background-color: #4caf50;
+  transition: width 0.3s ease;
 }
 </style>
--- 01-domain-driven-design/global-top.vue
+++ 01-domain-driven-design/global-top.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import Progress from './components/Progress.vue'
+import Progress from './components/Progress.vue';
 import { computed } from 'vue'
 
 const num = computed(() => {
-  const p = (($slidev.nav.currentPage - 2) / ($slidev.nav.total -2)) * 100
+  const p = (($slidev.nav.currentPage -1) / ($slidev.nav.total -1)) * 100
   return Math.floor(p)
 })
 </script>
 
 <template>
-  <div v-if="![1, 2, $slidev.nav.total + 1].includes($slidev.nav.currentPage)">
-    <div class="top absolute top-0 right-0">
-      <Progress :targetValue="num"/>
-    </div>
-  </div>
+<Progress :percentage="num"/>
 </template>
 
 <style>
